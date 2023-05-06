f2c72f30d4b9bbd666d1fd4b0d670991ef37bc9c
ldmax <lidanmax@163.com>
2023-05-06 16:29:53 +0800

    20230506

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -4,7 +4,9 @@ return {
     config = function()
       vim.g.copilot_proxy = "localhost:20171"
       -- completion key for github copilot
-      vim.g.copilot_assume_mapped = true
+      vim.g.copilot_no_tab_map = true
+      vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+      -- vim.g.copilot_assume_mapped = true
       -- disable filetypes
       vim.g.copilot_filetypes = { xml = false }
 
