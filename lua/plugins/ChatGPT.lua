7862c2f9224f539f1271442a5d27f2be29650d51
ldmax <lidanmax@163.com>
2023-05-13 03:39:38 +0000

    20230513 Vultr

--- lua/plugins/ChatGPT.lua
+++ lua/plugins/ChatGPT.lua
@@ -3,7 +3,22 @@ return {
     "jackMort/ChatGPT.nvim",
     event = "VeryLazy",
     config = function()
-      require("chatgpt").setup()
+      require("chatgpt").setup({
+        chat = {
+          keymaps = {
+            close = "<C-c>",
+            yank_last = "<C-y>",
+            scroll_up = "<C-u>",
+            scroll_down = "<C-d>",
+            toggle_settings = "<C-o>",
+            new_session = "<C-n>",
+            cycle_windows = "<Tab>",
+          },
+          popup_input = {
+            submit = "<C-j>",
+          },
+        },
+      })
     end,
     dependencies = {
       "MunifTanjim/nui.nvim",
