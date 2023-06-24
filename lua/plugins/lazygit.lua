f7fe80606b983ea1f2df7eb01bcc25735ba4323f
ldmax <lidanmax@163.com>
2023-06-24 21:08:33 +0800

    20230624 thinkpad

---
+++ lua/plugins/lazygit.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "kdheepak/lazygit.nvim",
+    commit = "3466e48439601445e26c65635421625886f2d0c0",
+    -- optional for floating window border decoration
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+  },
+}
