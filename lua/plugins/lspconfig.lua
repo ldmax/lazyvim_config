564b477b5000c2ae2b6fe7c32f3edf515cebce5e
ldmax <lidanmax@163.com>
2023-07-01 20:18:13 +0800

    20230701 thinkpad

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -2,9 +2,13 @@ return {
   "neovim/nvim-lspconfig",
   tag = "v0.1.6",
   pin = true,
-  opts = function(_, opts)
-    opts.diagnostics = {
+  opts = {
+    diagnostics = {
       virtual_text = false,
-    }
-  end,
+    },
+    servers = {
+      pyright = {},
+      tsserver = {},
+    },
+  },
 }
