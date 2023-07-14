ea4db4fb3ae107c04990ea440cc99b4e220300bf
ldmax <lidanmax@163.com>
2023-07-15 01:17:49 +0800

    20230715 Aliyun add Nvim-R

---
+++ lua/plugins/nvim-R.lua
@@ -0,0 +1,13 @@
+return {
+  "jalvesaq/Nvim-R",
+  ft = { "r", "rmd" },
+  tag = "v0.9.17",
+  pin = true,
+  config = function()
+    local status, nvimr = pcall(require, "Nvim-R")
+    if not status then
+      return
+    end
+    nvimr.setup({})
+  end,
+}
