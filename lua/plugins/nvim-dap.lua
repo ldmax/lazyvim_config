487f9c42ce348e46065ca55b2d3f3a02e093f515
Dan <lidanmax@163.com>
2023-12-01 15:51:13 +0800

    update dap related stuff

--- lua/plugins/nvim-dap.lua
+++ lua/plugins/nvim-dap.lua
@@ -1,45 +1,29 @@
 return {
   {
-    "mfussenegger/nvim-dap",
-    tag = "v0.7.0",
+    "rcarriga/nvim-dap-ui",
+    tag = "v3.9.1",
     pin = true,
-    config = function()
-      local py_exe = "/usr/bin/python3"
-      if vim.fn.exists("g:os") == 0 then
-        local is_windows = vim.fn.has("win32") == 1 or vim.fn.has("win64") == 1 or vim.fn.has("win16") == 1
-        if is_windows then
-          py_exe = "C:/Program Files/Python310/python.exe"
-        end
-      end
-      require("dap").adapters.python = {
-        type = "executable",
-        command = py_exe,
-        args = { "-m", "debugpy.adapter" },
-        enrich_config = function() end,
-      }
-
-      require("dap").configurations.python = {
-        {
-          -- The first three options are required by nvim-dap
-          type = "python", -- the type here established the link to the adapter definition: `dap.adapters.python`
-          request = "launch",
-          name = "Launch file",
-
-          -- Options below are for debugpy
-          -- see https://github.com/microsoft/debugpy/wiki/Debug-configuration-settingsfor
-          -- for supported options
-
-          program = "${file}", -- This configuration will launch the current file if used.
-          pythonPath = py_exe,
-        },
-      }
-    end,
+    -- stylua: ignore
     keys = {
-      { "<space>dt", "<cmd>DapToggleBreakpoint<cr>", desc = "Toggle breakpoint" },
-      { "<space>dc", "<cmd>DapContinue<cr>", desc = "Continue" },
-      { "<space>di", "<cmd>DapStepInto<cr>", desc = "Step into" },
-      { "<space>dr", "<cmd>DapStepOver<cr>", desc = "Step over" },
-      { "<space>do", "<cmd>DapStepOut<cr>", desc = "Step out" },
+      { "<leader>du", function() require("dapui").toggle({ }) end, desc = "Dap UI" },
+      { "<leader>de", function() require("dapui").eval() end, desc = "Eval", mode = {"n", "v"} },
     },
+    opts = {},
+    config = function(_, opts)
+      -- setup dap config by VsCode launch.json file
+      -- require("dap.ext.vscode").load_launchjs()
+      local dap = require("dap")
+      local dapui = require("dapui")
+      dapui.setup(opts)
+      dap.listeners.after.event_initialized["dapui_config"] = function()
+        dapui.open({})
+      end
+      dap.listeners.before.event_terminated["dapui_config"] = function()
+        dapui.close({})
+      end
+      dap.listeners.before.event_exited["dapui_config"] = function()
+        dapui.close({})
+      end
+    end,
   },
 }
