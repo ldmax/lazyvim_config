bc3c51793e5d13607810bf66d9728a18e297594a
ldmax <lidanmax@163.com>
2024-03-15 15:32:38 +0800

    add shortcut keymap

--- lua/plugins/calendar.lua
+++ lua/plugins/calendar.lua
@@ -3,5 +3,8 @@ return {
     "itchyny/calendar.vim",
     commit = "66e6d0955efb6beb68912dce3ff484e8805d975c",
     pin = true,
+    config = function()
+      vim.api.nvim_set_keymap("n", "<leader>cm", ":Calendar -month<CR>", { noremap = true })
+    end,
   },
 }
