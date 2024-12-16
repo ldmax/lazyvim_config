98d8a06e92f75cb986a0458c5cbd4d08786ae3eb
ldmax <lidanmax@163.com>
2024-12-16 11:46:57 +0800

    update according  to LazyVim update

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,14 +5,14 @@
 local opts = { noremap = true, silent = true }
 
 -- map <leader>; to dashboard
-vim.keymap.set("n", "<leader>;", "<CMD>Dashboard<CR>", opts)
+vim.keymap.set("n", "<leader>;", "<CMD>:lua Snacks.dashboard()<CR>", opts)
 
 -- Neorg shortcuts
-vim.keymap.set("n", "<leader>nt", "<CMD>Neorg workspace trading<CR>", opts)
-vim.keymap.set("n", "<leader>nw", "<CMD>Neorg workspace work<CR>", opts)
-vim.keymap.set("n", "<leader>nj", "<CMD>Neorg journal custom<CR>", opts)
-vim.keymap.set("n", "<leader>ni", "<CMD>Neorg inject-metadata<CR>", opts)
-vim.keymap.set("n", "<leader>nl", "<CMD>Neorg keybind all core.looking-glass.magnify-code-block<CR>", opts)
+vim.keymap.set("n", "<leader>wt", "<CMD>Neorg workspace trading<CR>", opts)
+vim.keymap.set("n", "<leader>ww", "<CMD>Neorg workspace work<CR>", opts)
+vim.keymap.set("n", "<leader>wj", "<CMD>Neorg journal custom<CR>", opts)
+vim.keymap.set("n", "<leader>wi", "<CMD>Neorg inject-metadata<CR>", opts)
+vim.keymap.set("n", "<leader>wl", "<CMD>Neorg keybind all core.looking-glass.magnify-code-block<CR>", opts)
 
 -- Telescope projects
 vim.keymap.set("n", "<leader>fp", "<CMD>Telescope projects<CR>", opts)
